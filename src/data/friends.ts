853ea0a56bdf90c358d42846734e337cfee9b9b4
Hikari Hayashi <rev.hikari@gmail.com>
2020-12-15 15:36:38 +0800

    Update

--- src/data/friends.ts
+++ src/data/friends.ts
@@ -5,41 +5,37 @@ const friends: Friend[] = [
     id: "imbushuo",
     name: "Bingxing Wang",
     homepage: "https://imbushuo.net/",
-    socials: {
-      github: "imbushuo",
-    },
+    socials: { github: "imbushuo" },
   },
   {
     id: "quanbrew",
     name: "Quan Brew",
     homepage: "https://ioover.net/",
-    socials: {
-      github: "quanbrew",
-    },
+    socials: { github: "quanbrew" },
   },
   {
     id: "sumimakito",
     name: "Makito",
     homepage: "https://keep.moe/",
-    socials: {
-      github: "SumiMakito",
-    },
+    socials: { github: "SumiMakito" },
+  },
+  {
+    id: "orzfly",
+    name: "Yeechan Lu",
+    homepage: "https://orzfly.com/",
+    socials: { github: "orzFly" },
   },
   {
     id: "artoria2e5",
     name: "Mingye Wang",
     homepage: "https://twitter.com/Artoria2e5",
-    socials: {
-      github: "Artoria2e5",
-    },
+    socials: { github: "Artoria2e5" },
   },
   {
     id: "wwyqianqian",
     name: "Qian Qian",
     homepage: "https://wwyqianqian.github.io/",
-    socials: {
-      github: "wwyqianqian",
-    },
+    socials: { github: "wwyqianqian" },
   },
 ];
 
