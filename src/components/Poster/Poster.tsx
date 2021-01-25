21e9c1d2099e863d39fe2df89a6edf7adcc2cf27
Hikari Hayashi <rev.hikari@gmail.com>
2021-01-26 01:43:31 +0800

    Use new URL

--- src/components/Poster/Poster.tsx
+++ src/components/Poster/Poster.tsx
@@ -1,13 +1,14 @@
 import type { ReactElement } from "react";
 
-import poster from "../../assets/poster.png";
 import styles from "./Poster.module.scss";
+
+const POSTER = new URL("../../assets/poster.png", import.meta.url).toString();
 
 const Poster = (): ReactElement => {
   return (
     <div className={styles.container}>
       <div className={styles.poster}>
-        <img className={styles.posterImage} src={poster} alt="" />
+        <img className={styles.posterImage} src={POSTER} alt="" />
       </div>
     </div>
   );
