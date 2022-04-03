be7b1f2ab471fa31dd0b2c5049ca46e2d9263321
Hikari Hayashi <rev.hikari@gmail.com>
2022-04-03 22:21:30 +0800

    Upgrade react

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,17 @@ import "./index.scss";
 
 import { BrowserRouter } from "@neetly/router";
 import { StrictMode } from "react";
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 
 import { App } from "./App";
 
-render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-  document.querySelector("#root"),
-);
+const container = document.querySelector("#root");
+if (container) {
+  createRoot(container).render(
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>,
+  );
+}
