e2ffefb3c04ffdd21f0388c3f0aeb973f007fc4f
Hikari Hayashi <rev.hikari@gmail.com>
2022-04-08 21:14:10 +0800

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import { Route, Routes } from "@neetly/router";
 
-import styles from "./App.module.scss";
 import { AppHeader } from "./components/AppHeader";
 import { HomePage } from "./pages/HomePage";
 
 const App = () => {
   return (
-    <div className={styles.container}>
+    <>
       <AppHeader />
       <Routes>
         <Route path="/" element={<HomePage />} />
       </Routes>
-    </div>
+    </>
   );
 };
 
