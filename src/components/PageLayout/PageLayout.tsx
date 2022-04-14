b2be02b2245ba2a4856ac6ff4ee29978e84fb8d9
Hikari Hayashi <rev.hikari@gmail.com>
2022-04-14 19:38:22 +0800

    Update

--- src/components/PageLayout/PageLayout.tsx
+++ src/components/PageLayout/PageLayout.tsx
@@ -1,12 +1,19 @@
 import type { ReactNode } from "react";
+import { useLayoutEffect } from "react";
 
+import { APP_NAME } from "../../constants";
 import styles from "./PageLayout.module.scss";
 
 type PageLayoutProps = {
+  title?: string;
   children?: ReactNode;
 };
 
-const PageLayout = ({ children }: PageLayoutProps) => {
+const PageLayout = ({ title, children }: PageLayoutProps) => {
+  useLayoutEffect(() => {
+    document.title = title ? `${title} - ${APP_NAME}` : APP_NAME;
+  }, [title]);
+
   return <main className={styles.layout}>{children}</main>;
 };
 
