f0bae11af39b8c5992b6d97ae77a21eb776758eb
Hikari Hayashi <rev.hikari@gmail.com>
2022-12-13 02:42:19 +0800

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { Outlet, ScrollRestoration } from "react-router-dom";
 
+import styles from "./App.module.scss";
+
 const App = () => {
   return (
-    <>
+    <div className={styles.app}>
+      <h1>Hikari Hayashi</h1>
       <Outlet />
       <ScrollRestoration />
-    </>
+    </div>
   );
 };
 
