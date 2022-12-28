98f2fe6f418b3b74d934727e82db8c6137729829
Hikari Hayashi <rev.hikari@gmail.com>
2022-12-28 23:18:53 +0800

    Update

--- src/components/SocialList/SocialItem.tsx
+++ src/components/SocialList/SocialItem.tsx
@@ -4,15 +4,15 @@ import styles from "./SocialItem.module.scss";
 
 type SocialItemProps = {
   name: ReactNode;
-  icon: string;
+  iconUrl: string;
   link: string;
 };
 
-const SocialItem = ({ name, icon, link }: SocialItemProps) => {
+const SocialItem = ({ name, iconUrl, link }: SocialItemProps) => {
   return (
     <a className={styles.link} href={link} target="_blank" rel="noreferrer">
       <span className={styles.item}>
-        <img className={styles.icon} src={icon} aria-hidden />
+        <img className={styles.icon} src={iconUrl} aria-hidden />
         <span className={styles.name}>{name}</span>
       </span>
     </a>
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -9,15 +9,23 @@ const HomePage = () => {
         <SocialList>
           <SocialItem
             name="Twitter"
-            icon={new URL(
+            iconUrl={new URL(
               "../../assets/icons/twitter.svg",
               import.meta.url,
             ).toString()}
             link="https://twitter.com/rev__hikari"
           />
           <SocialItem
+            name="Instagram"
+            iconUrl={new URL(
+              "../../assets/icons/instagram.svg",
+              import.meta.url,
+            ).toString()}
+            link="https://www.instagram.com/rev_hikari/"
+          />
+          <SocialItem
             name="GitHub"
-            icon={new URL(
+            iconUrl={new URL(
               "../../assets/icons/github.svg",
               import.meta.url,
             ).toString()}
@@ -26,36 +34,44 @@ const HomePage = () => {
         </SocialList>
       </section>
       <section>
+        <h1 className={styles.title}>Projects</h1>
+      </section>
+      <section>
         <h1 className={styles.title}>Friends</h1>
         <FriendList>
           <FriendItem
             name="Bingxing Wang"
-            avatarUrl="https://avatars.githubusercontent.com/u/6284353?s=96"
+            avatarUrl="https://avatars.githubusercontent.com/u/6284353?s=80"
             link="https://imbushuo.net/"
           />
           <FriendItem
             name="Jiayi Cheng"
-            avatarUrl="https://avatars.githubusercontent.com/u/8012410?s=96"
+            avatarUrl="https://avatars.githubusercontent.com/u/8012410?s=80"
             link="https://github.com/TundraWork"
           />
           <FriendItem
             name="Mingye Wang"
-            avatarUrl="https://avatars.githubusercontent.com/u/6459309?s=96"
+            avatarUrl="https://avatars.githubusercontent.com/u/6459309?s=80"
             link="https://twitter.com/Artoria2e5"
           />
           <FriendItem
             name="Tachibana Kiyomi"
-            avatarUrl="https://avatars.githubusercontent.com/u/34709816?s=96"
+            avatarUrl="https://avatars.githubusercontent.com/u/34709816?s=80"
             link="https://ioover.net/"
           />
           <FriendItem
             name="Yeechan Lu"
-            avatarUrl="https://avatars.githubusercontent.com/u/158528?s=96"
+            avatarUrl="https://avatars.githubusercontent.com/u/158528?s=80"
             link="https://orzfly.com/"
           />
           <FriendItem
+            name={<span lang="zh-Hans">谦谦</span>}
+            avatarUrl="https://avatars.githubusercontent.com/u/4645762?s=80"
+            link="https://xqq.im/"
+          />
+          <FriendItem
             name={<span lang="zh-Hans">三三</span>}
-            avatarUrl="https://avatars.githubusercontent.com/u/905663?s=96"
+            avatarUrl="https://avatars.githubusercontent.com/u/905663?s=80"
             link="https://best33.com/"
           />
         </FriendList>
