f35684969d4e9fdf871f6ced4302178913b88bfe
Hikari Hayashi <rev.hikari@gmail.com>
2025-02-05 03:29:14 +0800

    Update

---
+++ src/scripts/friends.js
@@ -0,0 +1,32 @@
+export const friends = [
+  {
+    name: { label: "Bingxing Wang", lang: "en" },
+    link: { href: "https://imbushuo.net/" },
+    avatar: { src: "https://avatars.githubusercontent.com/u/6284353?s={size}" },
+  },
+  {
+    name: { label: "Jiayi Cheng", lang: "en" },
+    link: { href: "https://github.com/TundraWork" },
+    avatar: { src: "https://avatars.githubusercontent.com/u/8012410?s={size}" },
+  },
+  {
+    name: { label: "Mingye Wang", lang: "en" },
+    link: { href: "https://x.com/Artoria2e5" },
+    avatar: { src: "https://avatars.githubusercontent.com/u/6459309?s={size}" },
+  },
+  {
+    name: { label: "Yeechan Lu", lang: "en" },
+    link: { href: "https://orzfly.com/" },
+    avatar: { src: "https://avatars.githubusercontent.com/u/158528?s={size}" },
+  },
+  {
+    name: { label: "谦谦", lang: "zh-Hans" },
+    link: { href: "https://github.com/xqq" },
+    avatar: { src: "https://avatars.githubusercontent.com/u/4645762?s={size}" },
+  },
+  {
+    name: { label: "三三", lang: "zh-Hans" },
+    link: { href: "https://best33.com/" },
+    avatar: { src: "https://avatars.githubusercontent.com/u/905663?s={size}" },
+  },
+];
