212b30099f4dafe220a97746ebcb1887784d10bc
Hikari Hayashi <rev.hikari@gmail.com>
2025-02-05 04:37:58 +0800

    Update

--- src/scripts/friends.js
+++ src/scripts/friends.js
@@ -2,31 +2,36 @@ export const friends = [
   {
     name: { label: "Bingxing Wang", lang: "en" },
     link: { href: "https://imbushuo.net/" },
-    avatar: { src: "https://avatars.githubusercontent.com/u/6284353?s={size}" },
+    avatar: { src: "https://avatars.githubusercontent.com/u/6284353?s=64" },
   },
   {
     name: { label: "Jiayi Cheng", lang: "en" },
     link: { href: "https://github.com/TundraWork" },
-    avatar: { src: "https://avatars.githubusercontent.com/u/8012410?s={size}" },
+    avatar: { src: "https://avatars.githubusercontent.com/u/8012410?s=64" },
   },
   {
     name: { label: "Mingye Wang", lang: "en" },
     link: { href: "https://x.com/Artoria2e5" },
-    avatar: { src: "https://avatars.githubusercontent.com/u/6459309?s={size}" },
+    avatar: { src: "https://avatars.githubusercontent.com/u/6459309?s=64" },
+  },
+  {
+    name: { label: "Tachibana Kiyomi", lang: "en" },
+    link: { href: "https://ioover.net/" },
+    avatar: { src: "https://avatars.githubusercontent.com/u/34709816?s=64" },
   },
   {
     name: { label: "Yeechan Lu", lang: "en" },
     link: { href: "https://orzfly.com/" },
-    avatar: { src: "https://avatars.githubusercontent.com/u/158528?s={size}" },
+    avatar: { src: "https://avatars.githubusercontent.com/u/158528?s=64" },
   },
   {
     name: { label: "谦谦", lang: "zh-Hans" },
     link: { href: "https://github.com/xqq" },
-    avatar: { src: "https://avatars.githubusercontent.com/u/4645762?s={size}" },
+    avatar: { src: "https://avatars.githubusercontent.com/u/4645762?s=64" },
   },
   {
     name: { label: "三三", lang: "zh-Hans" },
     link: { href: "https://best33.com/" },
-    avatar: { src: "https://avatars.githubusercontent.com/u/905663?s={size}" },
+    avatar: { src: "https://avatars.githubusercontent.com/u/905663?s=64" },
   },
 ];
