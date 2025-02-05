986cd1e2005536c25948fd6edaee011f543cdde0
Hikari Hayashi <rev.hikari@gmail.com>
2025-02-06 00:39:03 +0800

    Add Bluesky

--- src/scripts/socials.js
+++ src/scripts/socials.js
@@ -1,3 +1,4 @@
+import BlueksyIcon from "../components/social-icons/BlueskyIcon.astro";
 import GitHubIcon from "../components/social-icons/GitHubIcon.astro";
 import SteamIcon from "../components/social-icons/SteamIcon.astro";
 import XIcon from "../components/social-icons/XIcon.astro";
@@ -6,16 +7,21 @@ export const socials = [
   {
     name: { label: "X", lang: "en" },
     link: { href: "https://x.com/rev__hikari" },
-    Icon: XIcon,
+    icon: { Component: XIcon },
+  },
+  {
+    name: { label: "Bluesky", lang: "en" },
+    link: { href: "https://bsky.app/profile/hikari.im" },
+    icon: { Component: BlueksyIcon },
   },
   {
     name: { label: "GitHub", lang: "en" },
     link: { href: "https://github.com/latin-1" },
-    Icon: GitHubIcon,
+    icon: { Component: GitHubIcon },
   },
   {
     name: { label: "Steam", lang: "en" },
     link: { href: "https://steamcommunity.com/id/latin-1" },
-    Icon: SteamIcon,
+    icon: { Component: SteamIcon },
   },
 ];
