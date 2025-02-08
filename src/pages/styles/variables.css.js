15ea9b168dfe1087840d06712851a0189ec63337
Hikari Hayashi <rev.hikari@gmail.com>
2025-02-09 03:22:07 +0800

    Add dark mode

--- src/pages/styles/variables.css.js
+++ src/pages/styles/variables.css.js
@@ -9,6 +9,17 @@ const variables = {
       ]),
     ),
   ),
+
+  "color-text":
+    "light-dark(var(--color-neutral-800), var(--color-neutral-100))",
+  "color-text-dim":
+    "light-dark(var(--color-neutral-600), var(--color-neutral-300))",
+  "color-background":
+    "light-dark(var(--color-neutral-10), var(--color-neutral-900))",
+  "color-surface":
+    "light-dark(var(--color-neutral-50), var(--color-neutral-800))",
+  "color-primary":
+    "light-dark(var(--color-primary-600), var(--color-primary-300))",
 };
 
 export const GET = () => {
